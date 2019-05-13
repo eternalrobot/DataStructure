4df8d4c36b5c8e8342e92d272b63de5cc917bde3
eternalrobot <houd321@163.com>
2019-05-13 14:17:00 +0800

    循环队列的测试完毕，列表全部测试完毕

--- src/main/java/Queue/CircularQueue.java
+++ src/main/java/Queue/CircularQueue.java
@@ -5,14 +5,16 @@ public class CircularQueue<NodeType> extends Queue<NodeType>{
     private int nodenumber;//节点数量
     private int tail;//队尾指针
     private int head;//队头指针
-    private int initlength=100;//初始最大容量
-    private int addlength=50;//每次增加容量
+    private int initlength;//初始最大容量
+    private int addlength;//每次增加容量
 
     public CircularQueue(int initlength,int addlength) {
         this.initlength=initlength;
         this.addlength=addlength;
         try {
             nodenumber = 0;
+            tail=0;
+            head=0;
             nodes = (NodeType[]) new Object[initlength];
         } catch (Exception e) {
             System.out.println(e);
@@ -21,14 +23,10 @@ public class CircularQueue<NodeType> extends Queue<NodeType>{
 
     @Override
     boolean clear() {
-        try {
-            nodenumber = 0;
-            nodes = (NodeType[]) new Object[initlength];
-            return true;
-        } catch (Exception e) {
-            System.out.println(e);
-            return false;
-        }
+        tail=0;
+        head=0;
+        nodenumber=0;
+        return true;
     }
 
     @Override
@@ -73,5 +71,21 @@ public class CircularQueue<NodeType> extends Queue<NodeType>{
         head=(head+1)%nodes.length;
         nodenumber--;
         return T;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder=new StringBuilder();
+        if (nodenumber != 0) {
+            int Thead=head;
+            int Ttail=tail;
+            for (; (Thead+1)%nodes.length!=Ttail; Thead=(Thead+1)%nodes.length) {
+                stringBuilder.append(nodes[Thead] + ",");
+            }
+            stringBuilder.append(nodes[(Thead)%nodes.length]);
+            return "CircularQueue{"+stringBuilder+"},head{"+String.valueOf(head)+"},tail{"+String .valueOf(tail)+"}";
+        } else {
+            return "CircularQueue is empty,head{"+String.valueOf(head)+"},tail{"+String .valueOf(tail)+"}";
+        }
     }
 }
--- test/Queue/CircularQueueTest.java
+++ test/Queue/CircularQueueTest.java
@@ -7,28 +7,69 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 public class CircularQueueTest {
-
+    Queue<String> stringQueue;
     @Before
     public void setUp() throws Exception {
+        stringQueue=new CircularQueue<String>(100,50);
+        for(int i=0;i<50;i++){
+            stringQueue.enQueue(String.valueOf(i));
+        }
+        System.out.println(stringQueue);
     }
 
     @Test
     public void clear() {
+        stringQueue.clear();
+        System.out.println(stringQueue);
     }
 
     @Test
     public void isEmpty() {
+        System.out.println(stringQueue.isEmpty());
     }
 
     @Test
     public void length() {
+        System.out.println(stringQueue.length());
     }
 
     @Test
     public void enQueue() {
+        stringQueue.clear();
+        for(int i=0;i<100;i++){
+            stringQueue.enQueue(String.valueOf(i));
+        }
+        System.out.println(stringQueue);
+        stringQueue.deQueue();
+        stringQueue.enQueue("1000");
+        stringQueue.deQueue();
+        stringQueue.enQueue("1001");
+        stringQueue.deQueue();
+        stringQueue.deQueue();
+        stringQueue.enQueue("1002");
+        System.out.println(stringQueue);
+        stringQueue.clear();
+        for(int i=0;i<200;i++){
+            stringQueue.enQueue(String.valueOf(i));
+        }
+        System.out.println(stringQueue);
+        stringQueue.deQueue();
+        stringQueue.enQueue("1000");
+        stringQueue.deQueue();
+        stringQueue.enQueue("1001");
+        stringQueue.deQueue();
+        stringQueue.deQueue();
+        stringQueue.enQueue("1002");
+        System.out.println(stringQueue);
     }
 
     @Test
     public void deQueue() {
+        System.out.println(stringQueue.deQueue());
+        System.out.println(stringQueue);
+        System.out.println(stringQueue.deQueue());
+        System.out.println(stringQueue);
+        stringQueue.clear();
+        System.out.println(stringQueue.deQueue());
     }
 }
